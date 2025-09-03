bf0c4775fa095bd4d60bbe075f8c471b7e5b5412
reetusree <somasanireethushree05@gmail.com>
2025-09-03 12:36:05 +0530

    Add files via upload

---
+++ *Lab1P3.java
@@ -0,0 +1,12 @@
+import java.until.scannner;
+class lab1p3{
+ 	public static void main(string...args){
+ 		scanner s =new scannner(System.in);
+ 		try{
+ 		int a = s.nextInt();
+ 		int b = s.nextInt();
+ 		//var c =a/b;
+ 		System.out.println(a/b);
+	}Catch(	expection e){
+System.out.println(e.get class());
+}
