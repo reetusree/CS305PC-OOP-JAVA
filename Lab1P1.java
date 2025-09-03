410adf8db9c972aaaf2fb6e460ed2494e17ff5fb
reetusree <somasanireethushree05@gmail.com>
2025-09-03 11:50:19 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,60 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b =126;
+		Byte bb =b; //autoboxing
+		//int x =200;
+		//byte a=x;
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);
+		System.out.println("...............integer....");
+		
+		short s = 15;
+		Short ss =s;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.BYTES);
+		System.out.println("...............integer....");
+		
+		int i= 15;
+		Integer ii=i;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.SIZE);
+		System.out.println(ii.TYPE);
+		System.out.println(ii.BYTES);
+		System.out.println("...............integer....");
+		
+		long l=15;
+		Long ll=l;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(ll.SIZE);
+		System.out.println(ll.TYPE);
+	        System.out.println(ll.BYTES);
+	        System.out.println("...............integer....");
+		
+		float f=15;
+		Float ff=f;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(ff.MIN_VALUE);
+		System.out.println(ff.SIZE);
+		System.out.println(ff.TYPE);
+		System.out.println(ff.BYTES);
+		System.out.println("...............integer....");
+		
+		double d=15;
+		Double dd=d;
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		System.out.println(dd.BYTES);
+		System.out.println("...............integer....");
+		
+	}
+}
