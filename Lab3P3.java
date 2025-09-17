38403905d3a03d0e7dfcc4008dc57edd81cac1f1
reetusree <somasanireethushree05@gmail.com>
2025-09-17 12:31:32 +0530

    Add files via upload

---
+++ Lab3P3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Lab3P3{
+	public static void main(String...args){
+		Lab3P3 l = new Lab3P3();
+		l.display();l.display(10);l.display("String");l.display(10,"String");
+	}
+	public void show(){
+		System.out.println("method without params");
+	}
+	public void show(int x){
+		System.out.println("method without params:"+ x);
+	}
+	public void show(String s){
+		System.out.println("method without params:"+s);
+	}
+	public void show(int x,String s){
+		System.out.println("method with two params int and String:"+ x +" "+ s);
+	}
+		}
+		
+		
+
+
+		
+
+
+		
+
+
+		
+
+
+		
+		
